3c099123944d51d8de79f46c3ab5492fab568f96
jamshidbek-Ali-sherov <koruzadeilyas16@gmail.com>
2025-08-09 11:49:58 +0500

    bu esa mustaqil ish

---
+++ location_py.py
@@ -0,0 +1,20 @@
+# import phonenumbers
+# from phonenumbers import geocoder
+# phone_number1 = phonenumbers.parse("+998943851620")
+# phone_number2 = phonenumbers.parse("+79200323738")
+# phone_number3 = phonenumbers.parse("+998942061620")
+# phone_number4 = phonenumbers.parse("+998994426405")
+# print("\nphone Numbers location\n")
+# print(geocoder.description_for_number(phone_number1,"en"))
+# print(geocoder.description_for_number(phone_number2,"en"))
+# print(geocoder.description_for_number(phone_number3,"en"))
+# print(geocoder.description_for_number(phone_number4,"en"))
+text='hello world'
+shift=3
+alphabet='abcdefghjklomnpqrstuwxyz'
+input('nima:')
+for char in text.lower():
+     if alphabet==3:
+        print('d')
+else:
+        print('no')
---
+++ python_11.py
@@ -0,0 +1,82 @@
+# users=['john','johnny','jonathon']
+# password=str("root")
+#
+# while True:
+#   ism = input("ism:")
+#   parol = input("parol:")
+#
+#
+#   if ism in users and parol==password:
+#       print('xush kelibsiz')
+#       break
+#   else:
+#       print("ruxsat berilmadi")
+# users ={"doe":"root","john":"root2","Anna":"root3"}
+# while True:
+#   ism = input('ism')
+#   parol = input('parol')
+#
+#   if ism in users.keys():
+#     if users[ism] == parol:
+#       print('welcome sir')
+#     break
+#   else:
+#       print('it is a wrong!!!')
+# else:
+#       print('it is a wrong!!!')
+
+# 1. While siklidan foydalanib print qiling:
+# 1
+# 22
+# 333
+# 4444
+# 55555
+#
+#svetafor
+# sv=['qizil','yashil','sariq']
+# while True:
+#     rang=input('rangni tanlang:\n>>>')
+#     if rang in sv:
+#         print('mos keldi raxmat')
+#         continue
+#     else:
+#         print('kechirasiz xato')
+# sonni topish o'yini
+# import random
+# son=random.randint(1,10)
+# taxmin=int(input('raqam kiriting:'))
+# while True:
+#     if taxmin !=son:
+#         print('kechirasiz yana urining')
+#     else:
+#         print('tabriklaymiz davom eting')
+# do'stlar ro'yhati
+# friends=['alisher','qobil','misha','mirshohid']
+# name_1=input("do'stlaringizni tanlang:")
+# while True:
+#     if name_1 !='stop':
+#         print(friends)
+# valyuta ayirboshlash
+# pul = 1300  # int turida bo'lishi kerak
+#
+# while True:
+#     valyuta = input("Valyutangizni kiriting (exit - chiqish): 1 USD = 12600 so'm\n>>> ")
+#     if valyuta.lower() == 'exit':
+#         print('tamom')
+#         break  # chiqish uchun
+#     else:
+#         try:
+#             valyuta = float(valyuta)  # matnni son turiga o‘tkazamiz
+#             print(valyuta * pul, "so'm")
+#         except ValueError:
+#             print("Iltimos, son kiriting yoki 'exit' deb yozing.")
+#
+
+
+
+
+
+
+
+
+
